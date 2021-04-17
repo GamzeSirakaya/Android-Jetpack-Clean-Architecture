9855227cca0f0e785d8f28fa7ed8052f70a1f8c7
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-04-17 23:05:32 +0300

    Update MainActivityViewModel.kt

--- ViewModel&LiveData/ViewModel2/app/src/main/java/com/example/viewmodel2/MainActivityViewModel.kt
+++ ViewModel&LiveData/ViewModel2/app/src/main/java/com/example/viewmodel2/MainActivityViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.viewmodel2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
-    private var total = 0
+ private var total = MutableLiveData<Int>()
+    val totalData:LiveData<Int>
+    get()=total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal() = total
+
     fun setTotal(input: Int) {
-        total += input
+        total.value = (total.value)?.plus(input
+        )
     }
 
 
