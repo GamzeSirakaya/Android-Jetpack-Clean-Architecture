448cef8554031341ac3d419a1c43023e9ef69cd0
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-04-17 23:05:24 +0300

    Update MainActivityViewModel.kt

--- ViewModel&LiveData/ViewModel/app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
+++ ViewModel&LiveData/ViewModel/app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    private var count = 0
+    private var count =MutableLiveData<Int>()
+    val dataCount:LiveData<Int>
+        get() = count
+init {
+    count.value=0
+}
 
-    fun getCurrentCount() = count
-    fun getUpdateCuount() = ++count
+
+    fun updateCount(){
+        count.value = (count.value)?.plus(1)
+
+}
 }
