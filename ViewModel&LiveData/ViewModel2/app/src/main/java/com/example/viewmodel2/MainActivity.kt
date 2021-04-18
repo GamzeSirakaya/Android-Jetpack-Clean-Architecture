02fbd1e8b4a78dfabde10538b468d60b2b165f3f
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-04-19 00:38:18 +0300

    Update MainActivity.kt

--- ViewModel&LiveData/ViewModel2/app/src/main/java/com/example/viewmodel2/MainActivity.kt
+++ ViewModel&LiveData/ViewModel2/app/src/main/java/com/example/viewmodel2/MainActivity.kt
@@ -9,21 +9,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
     private lateinit var viewModelFactory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
-        viewModelFactory=MainActivityViewModelFactory(125)
-        viewModel=ViewModelProvider(this,viewModelFactory).get(MainActivityViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
         viewModel.totalData.observe(this, Observer {
-            binding.textView.text=it.toString()
-
+            binding.textView.text = it.toString()
         })
-        binding.button.setOnClickListener {
-            viewModel.setTotal(binding.editTextTextPersonName.text.toString().toInt())
-        }
+
 
     }
 }
