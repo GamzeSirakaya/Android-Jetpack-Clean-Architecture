9521ea0f85c16c7b75b6e4b709095a2d7e1fcdb3
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-04-20 23:35:48 +0300

    Create SecondFragment.kt

---
+++ Navigation/app/src/main/java/com/example/navigation/fragments/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigation.R
+import com.example.navigation.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+   private lateinit var binding:FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false)
+        var input= arguments?.getString("user_input")
+        binding.textView.text=input.toString()
+        return  binding.root
+    }
+
+}
